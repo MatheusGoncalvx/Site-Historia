b6a238ed1610a10421c43824a65f4cc198d0c672
MatheusGoncalvx <matheus.lima419@etec.sp.gov.br>
2024-09-03 17:26:05 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Caveat', 'sans-serif'], // Fonte padrão para sans-serif
+        lato: ['Lato', 'sans-serif'],  // Adicionando Lato
+      },
+      boxShadow: {
+        'custom-yellow': '0 4px 10px rgba(255, 223, 77, 0.5)', // Sombras personalizadas
+      },
+    },
+  },
+};
